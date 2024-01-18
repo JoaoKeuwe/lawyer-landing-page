7a65c045eafb2f82c1ee69ce6b01ee05a49a3a89
joaokeuwe <joaov2872@hotmail.com>
2024-01-18 01:53:33 -0300

    feat:

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
-import './App.css';
-import AboutUs from './components/about-us';
-import BlogSection from './components/blog-section';
-import Header from './components/header'
-import Hero from './components/hero';
+import "./App.css";
+import AboutUs from "./components/about-us";
+import BlogSection from "./components/blog-section";
+import Experience from "./components/experience";
+import Header from "./components/header";
+import Hero from "./components/hero";
 
 function App() {
   return (
     <div>
       <div>
-      <Header />
+        <Header />
       </div>
-      <div className='container-hero'>
+      <div className="container-hero">
         <Hero />
       </div>
       <div>
         <AboutUs />
       </div>
-      <div >
+      <div>
         <BlogSection />
+      </div>
+      <div>
+        <Experience />
       </div>
     </div>
   );
