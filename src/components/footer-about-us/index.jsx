8787f7217fdfceb1db8e575943f839adf85ae5aa
joaokeuwe <joaov2872@hotmail.com>
2024-01-18 01:08:59 -0300

    feat:

---
+++ src/components/footer-about-us/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./style.css"
+
+const FooterAboutUs = () => {
+  return (
+    <div className='container-footer-about'>
+      <div className='about-footer'>
+        <p> Atendimento presencial mediante <br /> <span className='span'> agendamento prévio</span></p>
+        <button>Contacte-nos</button>
+      </div>
+    </div>
+  )
+}
+
+export default FooterAboutUs
