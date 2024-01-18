7dcf5452ac51f6f22cea0201a05a6c1d13e3eb4a
joaokeuwe <joaov2872@hotmail.com>
2024-01-18 01:54:04 -0300

    feat:

--- src/components/footer-about-us/index.jsx
+++ src/components/footer-about-us/index.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
-import "./style.css"
+import React from "react";
+import "./style.css";
 
 const FooterAboutUs = () => {
   return (
-    <div className='container-footer-about'>
-      <div className='about-footer'>
-        <p> Atendimento presencial mediante <br /> <span className='span'> agendamento prévio</span></p>
-        <button>Contacte-nos</button>
+    <div className="container-footer-about">
+      <div className="about-footer">
+        <p>
+          {" "}
+          Atendimento presencial mediante <br />{" "}
+          <span className="span"> agendamento prévio</span>
+        </p>
+        <button>Agende já!</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default FooterAboutUs
+export default FooterAboutUs;
