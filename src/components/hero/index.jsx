61aef5bd8f04dc9ecd9a59a943e117c9c9c60773
joaokeuwe <joaov2872@hotmail.com>
2024-01-18 00:25:04 -0300

    feat:

---
+++ src/components/hero/index.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./style.css"
+
+const Hero = () => {
+    return (
+        <>
+            <div className="hero">
+                <div className="hero-content">
+                    <p className="p">Primeia reunião Gratuita</p>
+                    <h1>
+                        Serviços onlines <br /> & Presenciais
+                    </h1>
+                    <p className="paragraph">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi qui
+                        corrupti sapiente officia, repudiandae, numquam earum delectus
+                        explicabo provident eligendi eaque aut velit quaerat perspiciatis
+                        facilis illum veniam, tempora voluptatem.
+                    </p>
+                    <div className="button-group">
+                        <button className="button-information"> + Informações</button>
+
+                        <button className="button-more">Saber mais</button>
+                    </div>
+                </div>
+            </div>
+            <div className="know-more">
+                <p>
+                    Quer saber o que mais podemos fazer por você?
+                </p>
+            </div>
+        </>
+    );
+};
+
+export default Hero;
