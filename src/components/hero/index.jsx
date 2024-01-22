a3ea59a11ce275bbd25b705922c5b831b8cea66e
joaokeuwe <joaov2872@hotmail.com>
2024-01-22 02:20:14 -0300

    feat:

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -1,35 +1,32 @@
 import React from "react";
-import "./style.css"
+import "./style.css";
 
 const Hero = () => {
-    return (
-        <>
-            <div className="hero">
-                <div className="hero-content">
-                    <p className="p">Primeia reunião Gratuita</p>
-                    <h1>
-                        Serviços onlines <br /> & Presenciais
-                    </h1>
-                    <p className="paragraph">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi qui
-                        corrupti sapiente officia, repudiandae, numquam earum delectus
-                        explicabo provident eligendi eaque aut velit quaerat perspiciatis
-                        facilis illum veniam, tempora voluptatem.
-                    </p>
-                    <div className="button-group">
-                        <button className="button-information"> + Informações</button>
+  return (
+    <>
+      <div className="hero">
+        <div className="hero-content">
+          <p className="p">Primeia reunião Gratuita</p>
+          <h1>
+            Serviços onlines <br /> & Presenciais
+          </h1>
+          <p className="paragraph">
 
-                        <button className="button-more">Saber mais</button>
-                    </div>
-                </div>
-            </div>
-            <div className="know-more">
-                <p>
-                    Quer saber o que mais podemos fazer por você?
-                </p>
-            </div>
-        </>
-    );
+
+                      Descubra o direito mercantil conosco - seu escritório especializado. Oferecemos soluções personalizadas para empresas, com atendimento presencial e online. Da constituição ao litígio, nossa equipe está comprometida em fornecer suporte jurídico estratégico para impulsionar seus negócios. Confie em nós para proteger seus interesses no universo dinâmico do direito mercantil.
+          </p>
+          <div className="button-group">
+            <button className="button-information"> + Informações</button>
+
+            <button className="button-more">Saber mais</button>
+          </div>
+        </div>
+      </div>
+      <div className="know-more">
+        <p>Quer saber o que mais podemos fazer por você?</p>
+      </div>
+    </>
+  );
 };
 
 export default Hero;
